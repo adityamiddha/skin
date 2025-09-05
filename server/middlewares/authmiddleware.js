8345d4d79df6fb3353df5ffc586a56f37a73de40
Aditya Middha <your@email.com>
2025-09-05 12:14:52 +0530

    Update API base URL and env config

--- server/middlewares/authmiddleware.js
+++ server/middlewares/authmiddleware.js
@@ -6,29 +6,35 @@ const catchAsync = require('../../utils/catchAsync');
 exports.protect = catchAsync(async (req, res, next) => {
   let token;
 
-  // 1. Get token from headers
+  // 1. Try to get token from Authorization header
   if (
     req.headers.authorization &&
     req.headers.authorization.startsWith('Bearer')
   ) {
     token = req.headers.authorization.split(' ')[1];
+  } 
+  // 2. Otherwise, try to get token from cookies
+  else if (req.cookies && req.cookies.jwt) {
+    token = req.cookies.jwt;
   }
 
-  // 2. If no token, block access
+  // 3. If no token, block access
   if (!token) {
     return next(new AppError('You are not logged in!', 401));
   }
 
-  // 3. Verify token
+  // 4. Verify token
   const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-  // 4. Get user from decoded token
+  // 5. Get user from decoded token
   const user = await User.findById(decoded.id);
   if (!user) {
-    return next(new AppError('The user belonging to this token no longer exists.', 401));
+    return next(
+      new AppError('The user belonging to this token no longer exists.', 401)
+    );
   }
 
-  // 5. Attach user to request
+  // 6. Attach user to request
   req.user = user;
   next();
 });
@@ -36,7 +42,9 @@ exports.protect = catchAsync(async (req, res, next) => {
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     if (!roles.includes(req.user.role)) {
-      return next(new AppError('You do not have permission to perform this action', 403));
+      return next(
+        new AppError('You do not have permission to perform this action', 403)
+      );
     }
     next();
   };
