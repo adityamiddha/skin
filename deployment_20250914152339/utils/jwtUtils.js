e7522d955c56f5dd398ca5c2cce1dbd43e50c03f
Aditya Middha <adityamiddha7@gmail.com>
2025-09-14 15:28:23 +0530

    remove unwanted files.

--- deployment_20250914152339/utils/jwtUtils.js
+++
@@ -1,8 +0,0 @@
-const jwt = require('jsonwebtoken');
-
-// Function to create a JWT token with user ID
-exports.createToken = (userId) => {
-    return jwt.sign({id: userId}, process.env.JWT_SECRET, {
-        expiresIn: '7d',
-    });
-};
